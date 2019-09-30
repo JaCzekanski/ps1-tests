d1a63eec6a0cbcefa6bb22c2500907e6281f6eee
Jakub Czekański <ja.czekanski@gmail.com>
2019-09-30 21:22:14 +0200

    gpu/lines: added blending and polyline examples

--- README.md
+++ README.md
@@ -8,19 +8,23 @@ gpu/bandwidth        | Measure GPU/VRAM bandwidth
 gpu/benchmark        | Simple GPU test to benchmark rasteriser
 gpu/quad             | Semi-transparent polygon commands - for testing fill rules and transparency handling
 gpu/transparency     | Draws rectangles with 4 semi-transparent blending modes
-gpu/lines            | Draws horizontal and vertical opaque lines - for line rendering comparison
+gpu/lines            | Draws lines using different modes - for verifying Bresenham implementation, color blending, polyline handling
 gpu/texture-overflow | Draws textured rectangle with UV overflowing VRAM width
-gte-fuzz             | Executes GTE opcodes with random parameters, can be used to verify agains real console
-spu/test             | Check SPU behaviour (data is lost randomly on 32bit access, ok on 16bit)
+gte-fuzz             | Executes GTE opcodes with random parameters, can be used to verify against real console
+spu/test             | Check SPU behavior (data is lost randomly on 32bit access, ok on 16bit)
 spu/stereo           | Play samples on first two voices 
 timers               | Run Timer0,1,2 using various clock sources and sync modes and time them using busy loops and vblank interrupt
 
 Note: Make sure your PS-EXE loaded does set default value for Stack Pointer - these .exes has SP set to 0.
 
-
 ## Download
 
 [Latest release](https://github.com/JaCzekanski/ps1-tests/releases/latest)
+
+## Examples
+
+<img src="gpu/lines/vram.png" height="256">
+<img src="gpu/transparency/vram.png" height="256">
 
 ## Build
 
--- gpu/lines/main.c
+++ gpu/lines/main.c
@@ -4,6 +4,8 @@
 #include <psxgte.h>
 #include <psxetc.h>
 
+typedef char bool;
+
 DISPENV disp;
 DRAWENV draw;
 
@@ -42,6 +44,139 @@ void clearScreen() {
     fillRect(512, 256, 0x3f1, 256, 0xff, 0xff, 0xff);
 }
 
+void setE1(int transparencyMode, int dithering) {
+    DR_TPAGE e;
+    unsigned short texpage = getTPage(/* bitcount - do not care */0, /* semi-transparency mode */ transparencyMode, /*x*/0, /*y*/0);
+    setDrawTPage(&e, /* Drawing to display area */ 1, /* dithering */  dithering, texpage);
+    DrawPrim(&e);
+}
+
+void horizontalLines(int startX, int startY) {
+    for (int y = 0; y < 10; y++) {
+        LINE_F2 l;
+        setLineF2(&l);
+        setRGB0(&l, 0, 0, 0);
+
+        l.x0 = startX;
+        l.y0 = startY + y * 8;
+        l.x1 = startX + 80;
+        l.y1 = startY + y * 8 + y;
+
+        DrawPrim(&l);
+    }
+}
+
+void verticalLines(int startX, int startY) {
+    for (int x = 0; x < 10; x++) {
+        LINE_F2 l;
+        setLineF2(&l);
+        setRGB0(&l, 0, 0, 0);
+
+        l.x0 = startX + x * 8;
+        l.y0 = startY;
+        l.x1 = startX + x * 8 + x;
+        l.y1 = startY + 80;
+        
+        DrawPrim(&l);
+    }
+}
+
+
+void horizontalLines2(int startX, int startY) {
+    for (int y = 0; y < 20; y++) {
+        LINE_F2 l;
+        setLineF2(&l);
+        setRGB0(&l, 0, 0, 0);
+
+        l.x0 = startX;
+        l.y0 = startY + y * 4;
+        l.x1 = startX + y;
+        l.y1 = startY + y * 4 + 1;
+
+        DrawPrim(&l);
+    }
+}
+
+void flatLines(int startX, int startY)  {
+    for (int i = 0; i < 64; i++) {
+        LINE_F2 l;
+        setLineF2(&l);
+        setRGB0(&l, 0xaa, 0x00, 0x00);
+
+        l.x0 = startX;
+        l.y0 = startY + i;
+        l.x1 = startX + i;
+        l.y1 = startY + i;
+
+        DrawPrim(&l);
+    }
+}
+
+void gouraudLines(int startX, int startY)  {
+    for (int i = 0; i < 64; i++) {
+        LINE_G2 l;
+        setLineG2(&l);
+        setRGB0(&l, 0x00, 0x00, 0x00);
+        setRGB1(&l, 0xff, 0x00, 0x00);
+
+        l.x0 = startX;
+        l.y0 = startY + i;
+        l.x1 = startX + i;
+        l.y1 = startY + i;
+
+        DrawPrim(&l);
+    }
+}
+
+void flatMultiLine(int startX, int startY, bool transparent)  {
+    LINE_F4 l;
+    setLineF4(&l);
+    setSemiTrans(&l, transparent);
+
+    setRGB0(&l, 0xaa, 0, 0);
+
+    l.x0 = startX;
+    l.y0 = startY;
+
+    l.x1 = startX + 32;
+    l.y1 = startY;
+
+    l.x2 = startX + 32;
+    l.y2 = startY + 32;
+
+    l.x3 = startX;
+    l.y3 = startY;
+
+    DrawPrim(&l);
+}
+
+
+void gouraudMultiLine(int startX, int startY, bool transparent)  {
+    LINE_G4 l;
+    setLineG4(&l);
+    setSemiTrans(&l, transparent);
+
+    setRGB0(&l, 0xff, 0x00, 0x00);
+    setRGB1(&l, 0x00, 0xff, 0x00);
+    setRGB2(&l, 0x00, 0x00, 0xff);
+    setRGB2(&l, 0x00, 0xff, 0xff);
+
+    l.x0 = startX;
+    l.y0 = startY;
+
+    l.x1 = startX + 32;
+    l.y1 = startY;
+
+    l.x2 = startX + 32;
+    l.y2 = startY + 32;
+
+    l.x3 = startX;
+    l.y3 = startY;
+
+    DrawPrim(&l);
+}
+
+
 void circle(int cx, int cy, int scale, int segs) {
     const int PI = 4096/2;
 
@@ -68,46 +203,32 @@ int main() {
     printf("\ngpu/lines\n");
 
     for (;;) {
-        
         clearScreen();
+
+        // Dithering off
+        setE1(0, 0); 
+
+        horizontalLines(16, 16);
+        verticalLines(110, 16);
+        horizontalLines2(200, 16);
+
+        setE1(0, 0); 
+        flatLines(16, 100);
+        gouraudLines(16, 166);
         
-        LINE_F2 l;
-
-        setLineF2(&l);
-        setRGB0(&l, 0, 0, 0);
-
-        // Horizontal lines
-        for (int y = 0; y < 10; y++) {
-            l.x0 = 16;
-            l.y0 = 16 + y * 8;
-            l.x1 = 16 + 80;
-            l.y1 = 16 + y * 8 + y;
-
-            DrawPrim(&l);
-        }
+        // Dithering on
+        setE1(0, 1); 
         
-        // Vertical lines
-        for (int x = 0; x < 10; x++) {
-            l.x0 = 108 + x * 8;
-            l.y0 = 16;
-            l.x1 = 108 + x * 8 + x;
-            l.y1 = 16 + 80;
-            
-            DrawPrim(&l);
-        }
+        flatLines(84, 100);
+        gouraudLines(84, 166);
         
-        // Horizontal lines
-        for (int y = 0; y < 40; y++) {
-            l.x0 = 200;
-            l.y0 = 16 + y * 4;
-            l.x1 = 200 + 40 - y;
-            l.y1 = 17 + y * 4;
+        flatMultiLine(150, 100, false); // Opaque 
+        flatMultiLine(210, 100, true);  // Transparent 
 
-            DrawPrim(&l);
-        }
-        
+        gouraudMultiLine(150, 140, false); // Opaque 
+        gouraudMultiLine(210, 140, true);  // Transparent 
 
-        circle(SCR_W * 1/4, SCR_H * 4/6, 100, 6);
+        circle(200, 200, 140, 16);
 
         VSync(0);
     }
