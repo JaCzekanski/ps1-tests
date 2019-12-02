2fe1570401e6f66c66b4ce0355340f3822272445
Jakub Czekański <ja.czekanski@gmail.com>
2019-12-02 19:33:40 +0100

    Refactored gpu/transparency

--- common/stdint.h
+++ common/stdint.h
@@ -12,3 +12,4 @@ typedef int                int32_t;
 typedef unsigned char      uint8_t;
 typedef unsigned short int uint16_t;
 typedef unsigned int       uint32_t;
+typedef uint32_t           size_t;
--- gpu/transparency/main.c
+++ gpu/transparency/main.c
@@ -1,45 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <psxgpu.h>
-#include <psxetc.h>
-
-DISPENV disp;
-DRAWENV draw;
+#include <common.h>
 
 #define SCR_W 320
 #define SCR_H 240
-
-void setResolution(int w, int h) {
-    SetDefDispEnv(&disp, 0, 0, w, h);
-    SetDefDrawEnv(&draw, 0, 0, w, h);
-
-    PutDispEnv(&disp);
-    PutDrawEnv(&draw);
-}
-
-void initVideo()
-{
-    ResetGraph(0);
-    setResolution(SCR_W, SCR_H);
-    SetDispMask(1);
-}
-
-void fillRect(int x, int y, int w, int h, int r, int g, int b) {
-    FILL f;
-    setFill(&f);
-    setRGB0(&f, r, g, b);
-    setXY0(&f, x, y);
-    setWH(&f, w, h);
-
-    DrawPrim(&f);
-}
-
-void clearScreen() {
-    fillRect(0,   0,   512, 256, 0xff, 0xff, 0xff);
-    fillRect(512, 0,   512, 256, 0xff, 0xff, 0xff);
-    fillRect(0,   256, 512, 256, 0xff, 0xff, 0xff);
-    fillRect(512, 256, 0x3f1, 256, 0xff, 0xff, 0xff);
-}
 
 void setSemiTransparencyMode(int mode) {
     DR_TPAGE e;
@@ -53,7 +15,7 @@ const unsigned char bgColors[4] = {
 };
 
 int main() {
-    initVideo();
+    initVideo(SCR_W, SCR_H);
     printf("\ngpu/transparency\n");
 
     clearScreen();
