48a331a4712d536db101a1c932bdc6c2ae67f3e9
Connor McLaughlin <stenzek@gmail.com>
2020-04-28 15:57:37 +1000

    dma/chain-looping: Test how CPU behaves when invalid DMA chains are executed

--- common/dma.cpp
+++ common/dma.cpp
@@ -18,4 +18,30 @@ void masterEnable(Channel ch, bool enabled) {
     }
     write32(CONTROL_ADDR, dmaControl);
 }
+
+void channelIRQEnable(Channel ch, bool enabled) {
+    uint32_t dicr = read32(0x1F8010F4);
+    if (enabled)
+      dicr |= (1u << ((int)ch + 16)) + (1u << ((int)ch + 24));
+    else
+      dicr &= ~(1u << ((int)ch + 16));
+
+    write32(0x1F8010F4, dicr);
+}
+
+void masterIRQEnable(bool enabled) {
+    uint32_t dicr = read32(0x1F8010F4);
+    if (enabled)
+      dicr |= (1u << 23);
+    else
+      dicr &= ~(1u << 23);
+
+    write32(0x1F8010F4, dicr);
+}
+
+bool channelIRQSet(Channel ch) {
+  uint32_t dicr = read32(0x1F8010F4);
+  return (dicr & (1u << ((int)ch + 24))) != 0u;
+}
+
 };
