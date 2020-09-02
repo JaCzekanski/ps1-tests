e8cedc5edc4a9f8ed30ac1bb30d2b310983d0471
Jakub Czekański <ja.czekanski@gmail.com>
2020-09-02 23:47:27 +0200

    code-in-scratchpad: use exception handler (still crashing :( )

--- cpu/code-in-scratchpad/code.h
+++ cpu/code-in-scratchpad/code.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void FlushCache();
 uint32_t codeRam();
 uint32_t codeScratchpad();
+
+#ifdef __cplusplus
+}
+#endif
